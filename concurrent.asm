38ad7839f84a1655b69aa9ddcbdd691301c4b0d6
Rishi Sharma <rishi.id123@gmail.com>
2014-10-26 20:27:51 +0530

    Memory layout added

--- concurrent.asm
+++ concurrent.asm
@@ -1,1 +1,16 @@
-; First line added
+;	 		First line added
+; 			Memory Layout
+;
+;			8000H		____
+;			|	 |			|  3984 Bytes of memory.
+;			|	 |			|  (Reserved for Main program, User program's addresses)
+;			|	 |		____|
+;			8F8FH
+;			.	 .		____  
+;			.	 .		____|	Memory utilized by system.
+;			9000H		____
+;			|	 |			|  28761 Bytes of memory.
+;			|	 |			|  (User programs and their stacks)
+;			|	 |			|
+;			|	 |		____|
+;			FFFFH
